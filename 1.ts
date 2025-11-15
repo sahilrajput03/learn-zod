a43339b1a68ea6ce593afefc40a9cc3ad0d1b8d7
Sahil Rajput <sahilrajput03@gmail.com>
2025-11-15 16:08:46 +0530

    Update.

--- 1.ts
+++ 1.ts
@@ -4,9 +4,53 @@ import { z } from "zod";
 const mySchema = z.string();
 
 // parsing
-mySchema.parse("tuna"); // => "tuna"
-mySchema.parse(12); // => throws ZodError
+console.log(mySchema.parse("tuna")); // "tuna"
+// console.log(mySchema.parse(12)); // throws ZodError
 
 // "safe" parsing (doesn't throw error if validation fails)
-mySchema.safeParse("tuna"); // => { success: true; data: "tuna" }
-mySchema.safeParse(12); // => { success: false; error: ZodError }
+console.log(mySchema.safeParse("tuna")); // { success: true; data: "tuna" }
+console.log(mySchema.safeParse(12)); // { success: false; error: ZodError }
+
+
+// User
+console.log('\nUser 🚀')
+const User = z.object({
+    username: z.string(),
+    phone: z.number(),
+});
+console.log(User.parse({ username: "Ludwig", phone: 1234 })); // { username: 'Ludwig', phone: 1234 }
+
+const result = User.safeParse({ username: 42, phone: "100" });
+if (!result.success) {
+    // console.log("🚀 ~ result:", result.error) // ZodError instance
+    console.log("🚀 ~ result.error.issues:", result.error.issues)
+    /**
+    [
+        {
+            expected: 'string',
+            code: 'invalid_type',
+            path: [ 'username' ],
+            message: 'Invalid input: expected string, received number'
+        },
+        {
+            expected: 'number',
+            code: 'invalid_type',
+            path: [ 'phone' ],
+            message: 'Invalid input: expected number, received string'
+        }
+    ]
+     */
+
+    // console.log(z.prettifyError(result.error))
+    /**
+        ✖ Invalid input: expected string, received number
+          → at username
+        ✖ Invalid input: expected number, received string
+          → at phone
+     */
+} else {
+    result.data;
+}
+
+// ✅ Extract the inferred type
+type User = z.infer<typeof User>;
