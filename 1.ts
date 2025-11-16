703605aacde250628fd0d893c57d237b216e97d9
Sahil Rajput <sahilrajput03@gmail.com>
2025-11-16 22:56:56 +0530

    Update.

--- 1.ts
+++ 1.ts
@@ -100,4 +100,9 @@ type User = z.infer<typeof User>;
 
 
 // Create new schema with all fields optional
-const UserPartial = User.partial(); 
+const UserPartial = User.partial();
+
+
+// ❤️ Accessing a nested schema
+const CarSchema = z.object({ name: z.string(), price: z.number() });
+console.log(CarSchema.shape.name.parse("BMW")) // "BMW"
